86cf355c1a43052849ab9d5f7f17e947f9b5d32f
MunteanuRaresEugen <74292996+MunteanuRaresEugen@users.noreply.github.com>
2023-08-25 13:57:07 +0300

    created a mock-up ui

--- AutoInstaller/AutoInstaller/ViewModels/DemoViewModel.cs
+++ AutoInstaller/AutoInstaller/ViewModels/DemoViewModel.cs
@@ -1,5 +1,38 @@
-﻿namespace AutoInstaller.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
-public class DemoViewModel
-{
+namespace AutoInstaller.ViewModels;
+
+public sealed partial class DemoViewModel : ObservableObject
+{   
+    public DemoViewModel() 
+    {
+        //Options.Add(new Option() { Property = "APPDIR", PropertyName="ceva"});
+        //Options.Add(new Option() { Property = "APPDIR1", PropertyName = "ceva1" });
+        //Options.Add(new Option() { Property = "APPDIR2", PropertyName = "ceva2" });
+    }   
+    public partial class Option: ObservableObject
+    {
+        //[ObservableProperty]
+        //private string property;
+        //[ObservableProperty]
+        //private string propertyName;
+
+    } 
+    private ObservableCollection<Option> _options;
+    public ObservableCollection<Option> Options
+    {
+        get { return _options; }
+        set
+        {
+            if (_options != value)
+            {
+                _options = value;
+                OnPropertyChanged(nameof(Options));
+            }
+        }
+    }
+    
 }
+
