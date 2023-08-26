4dfc3b79e721816b150996ddeafb6ef521da6960
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-08-26 12:51:24 +0300

    solved the issue of parameters list not being bound to listbox

--- AutoInstaller/AutoInstaller/ViewModels/DemoViewModel.cs
+++ AutoInstaller/AutoInstaller/ViewModels/DemoViewModel.cs
@@ -5,34 +5,15 @@ using System.Collections.ObjectModel;
 namespace AutoInstaller.ViewModels;
 
 public sealed partial class DemoViewModel : ObservableObject
-{   
+{
+    public ObservableCollection<Parameter> Parameters { get; set; } = new ObservableCollection<Parameter>();
     public DemoViewModel() 
     {
-        //Options.Add(new Option() { Property = "APPDIR", PropertyName="ceva"});
-        //Options.Add(new Option() { Property = "APPDIR1", PropertyName = "ceva1" });
-        //Options.Add(new Option() { Property = "APPDIR2", PropertyName = "ceva2" });
-    }   
-    public partial class Option: ObservableObject
-    {
-        //[ObservableProperty]
-        //private string property;
-        //[ObservableProperty]
-        //private string propertyName;
-
-    } 
-    private ObservableCollection<Option> _options;
-    public ObservableCollection<Option> Options
-    {
-        get { return _options; }
-        set
-        {
-            if (_options != value)
-            {
-                _options = value;
-                OnPropertyChanged(nameof(Options));
-            }
-        }
+        Parameters.Add(new Parameter() { Name = "Appdir", Value = "C:\\users" });
     }
-    
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
 }
-
