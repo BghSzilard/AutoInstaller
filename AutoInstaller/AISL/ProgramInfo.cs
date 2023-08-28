1fadf404efd53fb83b8777c3e365b4be2eb4d10c
vio-p <viopuscas0430@yahoo.com>
2023-08-28 15:33:13 +0300

    Finished grammar definition and visitor, added unit tests

--- AutoInstaller/AISL/ProgramInfo.cs
+++ AutoInstaller/AISL/ProgramInfo.cs
@@ -2,16 +2,22 @@
 
 public struct ParameterInfo
 {
+    public bool IsOptional { get; set; }
     public string Type { get; set; }
     public string Name { get; set; }
-    public string DefaultValue { get; set; }
+    public string? DefaultValue { get; set; }
+    public string? FixedValue { get; set; }
+    public List<string>? Options { get; set; }
 
     public override bool Equals(object? obj)
     {
         return obj is ParameterInfo info &&
+               IsOptional == info.IsOptional &&
                Type == info.Type &&
                Name == info.Name &&
-               DefaultValue == info.DefaultValue;
+               DefaultValue == info.DefaultValue &&
+               FixedValue == info.FixedValue &&
+               (Options == null && Options == info.Options || Options != null && info.Options != null && Options.SequenceEqual(info.Options));
     }
 }
 
@@ -20,4 +26,6 @@ public class ProgramInfo
     public string Name { get; set; } = "";
     public string InstallationsPath { get; set; } = "";
     public List<ParameterInfo> ParameterList { get; set; } = new();
+    public bool Uninstall { get; set; }
+    public string InstallerPath { get; set; } = ""; // should it be here?
 }
