e80c9c1f2f0314d7e273d2a20e5e610806df60d0
BotiOpra <botioprabodi@gmail.com>
2023-08-30 00:11:47 +0300

    Added function to test exception handling

--- AutoInstaller/AISLTesting/AISLScriptVisitorTests.cs
+++ AutoInstaller/AISLTesting/AISLScriptVisitorTests.cs
@@ -1,14 +1,17 @@
 using AISL;
 using Antlr4.Runtime;
 using System.Text;
+using Xunit.Abstractions;
 
 namespace AISLTesting;
 
 public class AISLScriptVisitorTests
 {
+	private readonly ITestOutputHelper output;
+
     private readonly ProgramData _programData;
 
-    private const string ScriptPath = @"D:\Siemens\AutoInstaller\AutoInstaller\AISL\script.aisl";
+    private const string ScriptPath = @"C:\Users\Botond\Documents\Siemens\Summer School\Final\AutoInstaller\AutoInstaller\AISL\script.aisl";
 
     private List<ParameterData> ExpectedParameterList = new()
     {
@@ -20,8 +23,10 @@ public class AISLScriptVisitorTests
         new() { Type = "string", Name = "OptionalValue", DefaultValue = null, IsOptional = true, FixedValue = null, Options = null }
     };
 
-    public AISLScriptVisitorTests()
+    public AISLScriptVisitorTests(ITestOutputHelper output)
     {
+	    this.output = output;
+
         string scriptText = File.ReadAllText(ScriptPath, Encoding.UTF8);
 
         AntlrInputStream inputStream = new AntlrInputStream(scriptText);
@@ -56,6 +61,44 @@ public class AISLScriptVisitorTests
     public void InstallerPathIsValid()
     {
         Assert.Equal(@"D:\Siemens\tcb\230822_1.1.9_core\Simcenter Test Cloud Blueprint Setup.msi", _programData.InstallerPath);
+    }
+
+    public void ScriptWithIntentionalError_MissingLineOneSemicolon()
+    {
+	    string scriptText = """
+            FIND "Simcenter Test Cloud Blueprint" AT "D:\Siemens\tcb"
+            HAS (
+            	number Port WITH DEFAULT 8080,
+            	string ServerName,
+            	choice DropDown FROM ["option1", "option2"],
+            	flag Tick,
+            	string FixedParameter = "FixedValue",
+            	optional string OptionalValue
+            ) AS installation_parameters;
+            UNINSTALL "Simcenter Test Cloud Blueprint";
+            EXECUTE "D:\Siemens\tcb\230822_1.1.9_core\Simcenter Test Cloud Blueprint Setup.msi" WITH installation_parameters;
+            
+            """;
+
+	    AntlrInputStream inputStream = new AntlrInputStream(scriptText);
+	    AISLLexer aislLexer = new AISLLexer(inputStream);
+	    CommonTokenStream commonTokenStream = new CommonTokenStream(aislLexer);
+	    AISLParser aislParser = new AISLParser(commonTokenStream);
+        aislParser.AddErrorListener(new SyntaxErrorListener());
+	    //var errorHandler = aislParser.ErrorHandler;
+
+	    AISLParser.ScriptContext scriptContext = aislParser.script();
+	    AISLScriptVisitor visitor = new();
+        _ = visitor.Visit(scriptContext);
+
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnSyntaxError()
+    {
+	    Exception exception = Assert.Throws<Exception>(ScriptWithIntentionalError_MissingLineOneSemicolon);
+
+        output.WriteLine(exception.Message);
     }
 
     [Theory]
