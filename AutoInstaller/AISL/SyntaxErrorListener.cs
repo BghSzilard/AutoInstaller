9f1abd9c40c97e65a7b43ca206560c47292995f5
BotiOpra <botioprabodi@gmail.com>
2023-08-30 00:09:36 +0300

    Added SyntaxErrorListener, implemented necessary method

---
+++ AutoInstaller/AISL/SyntaxErrorListener.cs
@@ -0,0 +1,14 @@
+﻿namespace AISL;
+
+using Antlr4.Runtime;
+using System.IO;
+
+public class SyntaxErrorListener : BaseErrorListener
+{
+	public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine,
+		string msg, RecognitionException e)
+	{
+		//base.SyntaxError(output, recognizer, offendingSymbol, line, charPositionInLine, msg, e);
+		throw new Exception($"{line}:{charPositionInLine}: {msg}");
+	}
+}
