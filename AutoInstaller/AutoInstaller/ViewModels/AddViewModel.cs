04acd42cbf4ad45ae66c3b2910a25a6fe212b0e0
BotiOpra <botioprabodi@gmail.com>
2023-08-31 22:15:41 +0300

    Added validation, redesigned parameter options logic, removed unecessary properties and functions

--- AutoInstaller/AutoInstaller/ViewModels/AddViewModel.cs
+++ AutoInstaller/AutoInstaller/ViewModels/AddViewModel.cs
@@ -4,149 +4,164 @@ using Avalonia.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Core;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AutoInstaller.ViewModels;
 
-public partial class AddViewModel : ObservableObject
+public partial class AddViewModel : ObservableValidator
 {
-    public List<ParameterType> ParameterTypes { get; } = Enum.GetValues<ParameterType>().ToList();
+	public List<ParameterType> ParameterTypes { get; } = Enum.GetValues<ParameterType>().ToList();
 
-    [ObservableProperty] private string? _name;
-    [ObservableProperty] private string? _installationsPath;
-    [ObservableProperty] private string? _selectedVersion;
-    [ObservableProperty, NotifyCanExecuteChangedFor(nameof(AddProgramCommand))] private bool _isValid;
+	//installer settings
+	[ObservableProperty,
+	 NotifyPropertyChangedFor(nameof(HasName), nameof(AreInstallerDetailsSet)),
+	 NotifyCanExecuteChangedFor(nameof(AddProgramCommand)), Required(ErrorMessage = "Program Name cannot be empty")]
+	private string? _name;
 
-    public ObservableCollection<string> Versions { get; set; } = new();
-    public ObservableCollection<ParameterData> Parameters { get; set; } = new();
+	//todo add custom validator class
+	[ObservableProperty,
+	 NotifyPropertyChangedFor(nameof(HasValidDirectory), nameof(AreInstallerDetailsSet)),
+	 NotifyCanExecuteChangedFor(nameof(AddProgramCommand)), Required(ErrorMessage = "Directory cannot be empty")]
+	private string? _installationsPath;
 
-    [ObservableProperty] private ParameterData? _selectedParameter;
+	[ObservableProperty] private string? _selectedVersion;
 
-    [ObservableProperty] private ParameterType? _selectedParameterType = null;
-    [ObservableProperty] private string? _parameterName;
-    [ObservableProperty] private string? _parameterDefaultValue;
-    [ObservableProperty] private string? _parameterFixedValue;
-    [ObservableProperty] private bool _parameterIsOptional;
+	public bool HasName => !string.IsNullOrEmpty(Name);
 
-    [ObservableProperty] private bool _isWithoutValueChecked = true;
-    [ObservableProperty] private bool _isWithDefaultValueChecked = false;
-    [ObservableProperty] private bool _isWithFixedValueChecked = false;
+	// todo: simplify with De Morgan Theorem
+	public bool HasValidDirectory => ProgramService.CheckDirectoryValidity(InstallationsPath);
 
-    partial void OnInstallationsPathChanged(string? value)
-    {   
-        //if(string.IsNullOrEmpty(value))
-        //{
-        //    throw new DataValidationException("Invalid path");
-        //}
-        //Versions.Clear();
-        //if(ProgramService.FindVersionSubdirectories(value!).Count == 0)
-        //{
-        //    throw new DataValidationException("Invalid path");
-        //}
-        //else
-        //{
-            ProgramService.FindVersionSubdirectories(value!).ForEach(version => Versions.Add(version));
-        //}
-       
-    }
+	public bool AreInstallerDetailsSet => HasName && HasValidDirectory;
 
-    partial void OnNameChanged(string? value)
-    {   
-        if (string.IsNullOrEmpty(value))
-        {
-            IsValid = false;
-            throw new DataValidationException("Invalid Name");
-        }
+	// parameter settings
+	[ObservableProperty] private ParameterData? _selectedParameter;
+	[ObservableProperty] private ParameterType? _selectedParameterType = ParameterType.@string;
 
-        IsValid = true;
-    }
+	[ObservableProperty] 
+	[NotifyPropertyChangedFor(nameof(HasParameterName))]
+	[NotifyCanExecuteChangedFor(nameof(AddParameterCommand))]
+	[Required(ErrorMessage = "Parameter must have a name")]
+	private string? _parameterName;
 
-    partial void OnSelectedVersionChanged(string? value)
-    {   
-        // Not good yet
-        //if (string.IsNullOrEmpty(value))
-        //{
-        //    throw new NotImplementedException("Invalid Version");
-        //}
-    }
+	[ObservableProperty] private string? _parameterValue;
+	[ObservableProperty] private bool _parameterIsOptional;
+	[ObservableProperty] private bool _parameterIsReadOnly;
 
-    private readonly Window _window;
-    public AddViewModel(Window window)
-    {
-        _window = window;
-    }
+	public bool HasParameterName => !string.IsNullOrEmpty(ParameterName);
 
-    [RelayCommand(CanExecute = nameof(IsParameterDataValid))]
-    private void AddParameter()
-    {
-        ParameterData parameter = new()
-        {
-            IsOptional = ParameterIsOptional,
-            Type = SelectedParameterType!.Value, // make sure you can't add parameter without selecting a type
-            Name = ParameterName!,
-            DefaultValue = ParameterDefaultValue,
-            FixedValue = ParameterFixedValue
-        };
+	public ObservableCollection<string> Versions { get; set; } = new();
+	public ObservableCollection<ParameterData> Parameters { get; set; } = new();
 
-        Parameters.Add(parameter);
-    }
+	partial void OnInstallationsPathChanged(string? value)
+	{
+		Versions.Clear();
+		//if (string.IsNullOrEmpty(value))
+		//	throw new DataValidationException("Directory cannot be empty");
+		//if (!Directory.Exists(value))
+		//	throw new DataValidationException("Directory does not exist");
+		//if (ProgramService.FindVersionSubdirectories(value!).Count == 0)
+		//	throw new DataValidationException("Directory contains no Version folders");
+		if (ProgramService.CheckDirectoryValidity(value))
+		{
+			ProgramService.FindVersionSubdirectories(value!).ForEach(version => Versions.Add(version));
+			SelectedVersion = Versions[0];
+		}
 
-    [RelayCommand(CanExecute = nameof(IsParameterSelected))]
-    private void RemoveParameter()
-    {
-        Parameters.Remove(SelectedParameter!.Value);
-    }
+	}
 
-    [RelayCommand(CanExecute = nameof(IsProgramDataValid))]
-    private void AddProgram()
-    {
-        ProgramData programData = new() // remember to add data here
-        {
-            Name = Name,
-            InstallationsPath = InstallationsPath,
-            ParameterList = Parameters.ToList(),
-            Version = SelectedVersion,
-            Uninstall = true, // hardcoded for now, will be changed
-            InstallerPath = ProgramService.FindInstallerPath(Path.Combine(InstallationsPath!, SelectedVersion!))
-        };
-        ProgramService.SaveProgram(programData);
-    }
+	private readonly Window _window;
+	public AddViewModel(Window window)
+	{
+		_window = window;
+	}
 
-    [RelayCommand]
-    private async Task SelectInstallationsFolder()
-    {
-        var dialog = new OpenFolderDialog
-        {
-            Title = "Select a folder",
-        };
+	// todo: simplify logic by having a single Value property and a bool that checks if the user wants a ReadOnly parameter Value (i.e. FixedValue) or ReadWrite (i.e. DefaultValue)
+	[RelayCommand(CanExecute = nameof(IsParameterDataValid))]
+	private void AddParameter()
+	{
+		ParameterData parameter;
+		if (ParameterIsReadOnly)
+		{
+			parameter = new ParameterData
+			{
+				IsOptional = ParameterIsOptional,
+				Type = SelectedParameterType!.Value, // make sure you can't add parameter without selecting a type
+				Name = ParameterName!,
+				DefaultValue = null,
+				FixedValue = ParameterValue
+			};
+		}
+		else
+		{
+			parameter = new ParameterData
+			{
+				IsOptional = ParameterIsOptional,
+				Type = SelectedParameterType!.Value, // make sure you can't add parameter without selecting a type
+				Name = ParameterName!,
+				DefaultValue = ParameterValue,
+				FixedValue = null
+			};
+		}
 
-        var selectedFolder = await dialog.ShowAsync(_window);
+		Parameters.Add(parameter);
+	}
 
-        if (!string.IsNullOrEmpty(selectedFolder))
-        {
-            InstallationsPath = selectedFolder;
-        }
-    }
+	[RelayCommand(CanExecute = nameof(IsParameterSelected))]
+	private void RemoveParameter()
+	{
+		Parameters.Remove(SelectedParameter!.Value);
+	}
 
-    private bool IsParameterDataValid()
-    {
-        //IsValid = !string.IsNullOrEmpty(Name);
-     
-        return true;
-    }
+	[RelayCommand(CanExecute = nameof(CanAddProgram))]
+	private void AddProgram()
+	{
+		ProgramData programData = new() // remember to add data here
+		{
+			Name = Name,
+			InstallationsPath = InstallationsPath,
+			ParameterList = Parameters.ToList(),
+			Version = SelectedVersion,
+			Uninstall = true, // hardcoded for now, will be changed
+			InstallerPath = ProgramService.FindInstallerPath(Path.Combine(InstallationsPath!, SelectedVersion!))
+		};
+		ProgramService.SaveProgram(programData);
+	}
 
-    private bool IsProgramDataValid()
-    {
-        return IsValid;
-    }
+	[RelayCommand]
+	private async Task SelectInstallationsFolder()
+	{
+		var dialog = new OpenFolderDialog
+		{
+			Title = "Select a folder",
+		};
 
-    private bool IsParameterSelected()
-    {
-        return (SelectedParameter != null);
-    }
+		var selectedFolder = await dialog.ShowAsync(_window);
+
+		if (!string.IsNullOrEmpty(selectedFolder))
+		{
+			InstallationsPath = selectedFolder;
+		}
+	}
+
+	private bool IsParameterDataValid()
+	{
+		return HasParameterName;
+	}
+
+	private bool CanAddProgram()
+	{
+		return HasName && HasValidDirectory;
+	}
+
+	private bool IsParameterSelected()
+	{
+		return SelectedParameter != null;
+	}
 }
